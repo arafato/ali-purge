a0f74ae4de7935fe01b81045e0ee6cdccdc5bf49
arafato <oliver.arafat@outlook.com>
2023-03-28 20:31:52 +0200

    added logging

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,62 @@
 package cmd
 
 import (
+	"log"
+	"time"
+
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func NewRootCommand() *cobra.Command {
 	var verbose bool
+	var logger *zap.SugaredLogger
 
 	command := &cobra.Command{
 		Use:   "ali-purge",
 		Short: "ali-purge removes every resource from your Alibaba Cloud account.",
-		Long:  `A tool which removes every resource from an Alibaba Cloud account.  Use it with caution, since it cannot distinguish between production and non-production.`,
+		Long: `
+ @@@@@@   @@@       @@@  @@@@@@@   @@@  @@@  @@@@@@@    @@@@@@@@  @@@@@@@@  
+ @@@@@@@@  @@@       @@@  @@@@@@@@  @@@  @@@  @@@@@@@@  @@@@@@@@@  @@@@@@@@  
+ @@!  @@@  @@!       @@!  @@!  @@@  @@!  @@@  @@!  @@@  !@@        @@!       
+ !@!  @!@  !@!       !@!  !@!  @!@  !@!  @!@  !@!  @!@  !@!        !@!       
+ @!@!@!@!  @!!       !!@  @!@@!@!   @!@  !@!  @!@!!@!   !@! @!@!@  @!!!:!    
+ !!!@!!!!  !!!       !!!  !!@!!!    !@!  !!!  !!@!@!    !!! !!@!!  !!!!!:    
+ !!:  !!!  !!:       !!:  !!:       !!:  !!!  !!: :!!   :!!   !!:  !!:       
+ :!:  !:!   :!:      :!:  :!:       :!:  !:!  :!:  !:!  :!:   !::  :!:       
+ ::   :::   :: ::::   ::   ::       ::::: ::  ::   :::   ::: ::::   :: ::::  
+  :   : :  : :: : :  :     :         : :  :    :   : :   :: :: :   : :: ::   
+																			 
+ A tool which removes every resource from an Alibaba Cloud account.  
+ Use it with caution, since it cannot distinguish between production and non-production!`,
+	}
+
+	command.PreRun = func(cmd *cobra.Command, args []string) {
+		loggerConfig := zap.NewProductionConfig()
+		loggerConfig.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout(time.RFC3339)
+
+		if verbose {
+			loggerConfig.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+		} else {
+			loggerConfig.Level = zap.NewAtomicLevel() //Info Level by default
+		}
+
+		_logger, err := loggerConfig.Build()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer _logger.Sync()
+		logger = _logger.Sugar()
+		logger.Debug("Logger initialized")
+	}
+
+	command.RunE = func(cmd *cobra.Command, args []string) error {
+		// TODO: Initialize Alicloud credentials
+		// TODO: Read configuration file
+		// TODO: Start purging
+		return nil
 	}
 
 	command.PersistentFlags().BoolVarP(
